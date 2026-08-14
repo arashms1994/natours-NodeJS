fdaadb077fee0994082254b20e52031ade1b693a
arashms1994 <arash.ms1994@gmail.com>
2026-08-14 19:00:47 +0330

    Feat: Add tours get api from tours simple file

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 import express from 'express';
+import { readFileSync } from 'fs';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You Can Post To this Endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You Can Post To this Endpoint...');
+// });
+
+const tours = JSON.parse(readFileSync('./dev-data/data/tours-simple.json'));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    staus: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(port, () => {
