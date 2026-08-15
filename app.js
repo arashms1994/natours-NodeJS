006a9855b5c99b42ea6e2cc12e5439420bb6a249
arashms1994 <arash.ms1994@gmail.com>
2026-08-15 23:18:14 +0330

    Feat: Add Post Method and get Tour By ID

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFile } from 'fs';
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res
@@ -24,6 +25,44 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((t) => t.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      staus: 'fail',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    staus: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 app.listen(port, () => {
