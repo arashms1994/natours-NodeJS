bff3985ab681a46c84cf563ed89d900e3063d77d
arashms1994 <arash.ms1994@gmail.com>
2026-08-16 23:27:17 +0330

    Fix: Refactor code and add controller files

---
+++ controllers/tourController.js
@@ -0,0 +1,73 @@
+import { readFileSync, writeFile } from 'fs';
+
+const tours = JSON.parse(readFileSync('./dev-data/data/tours-simple.json'));
+
+export const checkID = (req, res, next, val) => {
+  console.log(`ID is ${val}`);
+
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      staus: 'fail',
+      message: 'invalid ID',
+    });
+  }
+  next();
+};
+
+export const getAllTours = (req, res) => {
+  res.status(200).json({
+    staus: 'success',
+    requestAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+export const getTourByID = (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((t) => t.id === id);
+
+  res.status(200).json({
+    staus: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+export const deleteTour = (req, res) => {
+  res.status(204).json({
+    staus: 'success',
+    data: null,
+  });
+};
+
+export const updateTour = (req, res) => {
+  res.status(200).json({
+    staus: 'success',
+    data: {
+      tour: '<Updated Tour....>',
+    },
+  });
+};
+
+export const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+};
---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+export const getAllUsers = (req, res) => {
+  res.status(500).json({
+    staus: 'error',
+    message: 'This Route is not yet defined!',
+  });
+};
+
+export const getUserByID = (req, res) => {
+  res.status(500).json({
+    staus: 'error',
+    message: 'This Route is not yet defined!',
+  });
+};
+
+export const deleteUser = (req, res) => {
+  res.status(500).json({
+    staus: 'error',
+    message: 'This Route is not yet defined!',
+  });
+};
+
+export const updateUser = (req, res) => {
+  res.status(500).json({
+    staus: 'error',
+    message: 'This Route is not yet defined!',
+  });
+};
+
+export const createUser = (req, res) => {
+  res.status(500).json({
+    staus: 'error',
+    message: 'This Route is not yet defined!',
+  });
+};
