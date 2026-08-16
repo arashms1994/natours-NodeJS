6dac083a9dbfe3a9336013af501125b81134054f
arashms1994 <arash.ms1994@gmail.com>
2026-08-17 00:25:23 +0330

    Feat: Add and test static pages with express

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ import { userRouter } from './routes/userRoutes.js';
 export const app = express();
 
 app.use(morgan('dev'));
-
 app.use(express.json());
+
+app.use(express.static('./public'));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
