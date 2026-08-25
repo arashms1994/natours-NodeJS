fd395ce9add7f08f2266ddf5d855a0e5cf076174
arashms1994 <arash.ms1994@gmail.com>
2026-08-25 23:58:27 +0330

    Fix: refactor CRUD actions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,39 +1,78 @@
 import { Tour } from '../models/tourModel.js';
 
-export const getAllTours = (req, res) => {
-  // res.status(200).json({
-  //   staus: 'success',
-  //   requestAt: req.requestTime,
-  //   results: tours.length,
-  //   data: {
-  //     tours,
-  //   },
-  // });
+export const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      staus: 'success',
+      requestAt: req.requestTime,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      staus: 'fail',
+      message: err,
+    });
+  }
 };
 
-export const getTourByID = (req, res) => {
-  res.status(200).json({
-    staus: 'success',
-    // data: {
-    //   tour,
-    // },
-  });
+export const getTourByID = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      staus: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      staus: 'fail',
+      message: err,
+    });
+  }
 };
 
-export const deleteTour = (req, res) => {
-  res.status(204).json({
-    staus: 'success',
-    data: null,
-  });
+export const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      staus: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      staus: 'fail',
+      message: err,
+    });
+  }
 };
 
-export const updateTour = (req, res) => {
-  res.status(200).json({
-    staus: 'success',
-    data: {
-      tour: '<Updated Tour....>',
-    },
-  });
+export const updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      staus: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      staus: 'fail',
+      message: err,
+    });
+  }
 };
 
 export const createTour = async (req, res) => {
